ae2de6a29c6b6bcd4d0779e9cf7cfa3862190efb
Maulana <maulanaajk@gmail.com>
2024-02-24 14:35:21 +0700

    style: button focus-within

--- components/button.tsx
+++ components/button.tsx
@@ -9,12 +9,13 @@ export default function Button<T extends React.ElementType = 'button'>({
     ...props
 }: ButtonProps<T> &
     Omit<React.ComponentPropsWithoutRef<T>, keyof ButtonProps<T>>) {
-    const Component = as || 'button';
+    const Component = as ?? 'button';
     return (
         <Component
             {...props}
             className={cn(
-                'group flex items-center justify-center gap-3 overflow-hidden whitespace-nowrap rounded-full bg-white p-3 ring-2 ring-gray-200 ring-opacity-30 transition-all duration-300 hover:ring-4 dark:text-black',
+                'group flex items-center justify-center gap-3 overflow-hidden whitespace-nowrap rounded-full bg-white p-3 transition-all duration-300',
+                'ring-2 ring-gray-200/45 focus-within:outline-none focus-within:ring-4 hover:ring-4 dark:text-black  dark:ring-gray-200/30',
                 props.className
             )}
         />
