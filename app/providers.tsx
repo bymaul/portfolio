dfacbd1da3f8a553c03460d81c7adc4dfdb4c716
Maulana <maulanaajk@gmail.com>
2024-03-02 01:15:37 +0700

    refactor(ThemeProvider): props to be readonly

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,6 +3,9 @@
 import { ThemeProvider as NextThemeProvider } from 'next-themes';
 import { ThemeProviderProps } from 'next-themes/dist/types';
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+export function ThemeProvider({
+    children,
+    ...props
+}: Readonly<ThemeProviderProps>) {
     return <NextThemeProvider {...props}>{children}</NextThemeProvider>;
 }
