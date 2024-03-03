ea6af49f6b25de325383075c5aa5395b993600d1
Maulana <maulanaajk@gmail.com>
2024-03-03 21:42:06 +0700

    refactor(location): show nav control when map loaded

--- components/grid/location.tsx
+++ components/grid/location.tsx
@@ -6,6 +6,7 @@ import { FaMinus, FaPlus } from 'react-icons/fa6';
 import Map, { MapRef } from 'react-map-gl';
 import Button from '../button';
 import Card from '../card';
+import { set } from 'date-fns';
 
 const maxZoom: number = 11;
 const minZoom: number = 4;
@@ -14,6 +15,7 @@ export default function Location() {
     const mapboxToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
     const [currentZoom, setCurrentZoom] = useState<number>(maxZoom);
     const [isButtonDisabled, setIsButtonDisabled] = useState<boolean>(false);
+    const [isMapLoaded, setIsMapLoaded] = useState<boolean>(false);
 
     const mapRef = useRef<MapRef>(null);
 
@@ -61,6 +63,9 @@ export default function Location() {
                     pitchWithRotate={false}
                     touchZoomRotate={false}
                     antialias={true}
+                    onLoad={() => {
+                        setIsMapLoaded(true);
+                    }}
                     initialViewState={{
                         latitude: -7.7962967,
                         longitude: 110.3667211,
@@ -68,30 +73,32 @@ export default function Location() {
                     }}
                     maxZoom={maxZoom}
                     minZoom={minZoom}>
-                    <div className='absolute inset-x-3 bottom-3 flex items-center justify-between'>
-                        <Button
-                            className={
-                                currentZoom === minZoom
-                                    ? 'invisible'
-                                    : 'cancel-drag'
-                            }
-                            aria-label='Zoom Out'
-                            type='button'
-                            onClick={handleZoomOut}>
-                            <FaMinus />
-                        </Button>
-                        <Button
-                            className={
-                                currentZoom === maxZoom
-                                    ? 'invisible'
-                                    : 'cancel-drag'
-                            }
-                            aria-label='Zoom In'
-                            type='button'
-                            onClick={handleZoomIn}>
-                            <FaPlus />
-                        </Button>
-                    </div>
+                    {isMapLoaded && (
+                        <div className='absolute inset-x-3 bottom-3 flex items-center justify-between'>
+                            <Button
+                                className={
+                                    currentZoom === minZoom
+                                        ? 'invisible'
+                                        : 'cancel-drag'
+                                }
+                                aria-label='Zoom Out'
+                                type='button'
+                                onClick={handleZoomOut}>
+                                <FaMinus />
+                            </Button>
+                            <Button
+                                className={
+                                    currentZoom === maxZoom
+                                        ? 'invisible'
+                                        : 'cancel-drag'
+                                }
+                                aria-label='Zoom In'
+                                type='button'
+                                onClick={handleZoomIn}>
+                                <FaPlus />
+                            </Button>
+                        </div>
+                    )}
                 </Map>
             </div>
         </Card>
