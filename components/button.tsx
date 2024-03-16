58177837e8d4b9649ceca17cd48e2c9637d28c4c
Maulana <maulanaajk@gmail.com>
2024-03-17 01:11:50 +0700

    style: fix outline flick on button click

--- components/button.tsx
+++ components/button.tsx
@@ -14,8 +14,8 @@ export default function Button<T extends React.ElementType = 'button'>({
         <Component
             {...props}
             className={cn(
-                'group flex items-center justify-center gap-3 overflow-hidden whitespace-nowrap rounded-full bg-white p-3 transition-all duration-300',
-                'ring-2 ring-gray-200/45 focus-within:outline-none focus-within:ring-4 hover:ring-4 dark:text-black  dark:ring-gray-200/30',
+                'group inline-flex items-center justify-center gap-3 overflow-hidden whitespace-nowrap rounded-full bg-white p-3 transition-all duration-300',
+                'outline-none ring-2 ring-gray-200/45 focus-within:outline-none focus-within:ring-4 hover:ring-4 dark:text-black dark:ring-gray-200/30',
                 props.className
             )}
         />
