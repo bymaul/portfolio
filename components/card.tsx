c33fe8efe8d185be77a0a84b1649be807ed955a1
Maulana <maulanaajk@gmail.com>
2024-04-20 11:19:57 +0700

    style(grid): add cursor grabbing on card drag

--- components/card.tsx
+++ components/card.tsx
@@ -9,7 +9,8 @@ export default function Card({ className, children }: CardProps) {
     return (
         <div
             className={cn(
-                'h-full cursor-grab select-none overflow-hidden rounded-3xl bg-white shadow-sm transition-shadow duration-500 hover:shadow-lg dark:bg-dark-900',
+                'h-full cursor-grab select-none overflow-hidden rounded-3xl bg-white active:cursor-grabbing dark:bg-dark-900',
+                'shadow-sm transition-shadow duration-500 hover:shadow-lg',
                 className
             )}>
             {children}
