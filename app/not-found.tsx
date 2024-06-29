4f42dc1c57bf9c03ef423bb1c516665b509e294f
Maulana <maulanaajk@gmail.com>
2024-06-29 23:51:00 +0700

    refactor: update not-found page layout and styles

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Button from '@/components/button';
+import Container from '@/components/container';
 import Link from 'next/link';
 import { useLayoutEffect, useState } from 'react';
 import { FaArrowRight } from 'react-icons/fa6';
@@ -9,30 +10,30 @@ export default function NotFound() {
     const [isMounted, setIsMounted] = useState(false);
 
     useLayoutEffect(() => {
-        setTimeout(() => {
-            setIsMounted(true);
-        }, 0);
+        setIsMounted(true);
     }, []);
 
     return (
-        <div
-            style={{
-                opacity: isMounted ? 1 : 0,
-                transform: isMounted ? 'translateY(0)' : 'translateY(20px)',
-                transition: 'opacity 500ms, transform 500ms',
-            }}
-            className='flex min-h-screen flex-col items-center justify-center gap-4 px-4'>
-            <h1 className='text-center text-9xl font-bold'>404</h1>
-            <h2 className='text-center text-3xl font-semibold'>
-                Page Not Found
-            </h2>
-            <p className='text-center'>
-                Sorry, we couldn&apos;t find what you were looking for.
-            </p>
-            <Button as={Link} className='px-4 py-2' href='/'>
-                <FaArrowRight className='-rotate-45 transition-transform duration-300 group-hover:-rotate-180' />
-                Back to Home
-            </Button>
-        </div>
+        <Container
+            as='main'
+            className='flex min-h-screen items-center justify-center'>
+            <div
+                className='space-y-4 text-center'
+                style={{
+                    opacity: isMounted ? 1 : 0,
+                    transform: isMounted ? 'translateY(0)' : 'translateY(20px)',
+                    transition: 'opacity 500ms, transform 500ms',
+                }}>
+                <h1 className='text-7xl font-bold md:text-9xl'>404</h1>
+                <h2 className='text-xl font-semibold md:text-3xl'>
+                    Page Not Found
+                </h2>
+                <p>Sorry, we couldn&apos;t find what you were looking for.</p>
+                <Button as={Link} href='/' className='px-4 py-2'>
+                    <FaArrowRight className='-rotate-45 transition-transform duration-300 group-hover:-rotate-180' />
+                    Back to Home
+                </Button>
+            </div>
+        </Container>
     );
 }
