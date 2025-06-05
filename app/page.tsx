c2a2ba179d1bc75b1e030c46ce1b986fe9ea5a91
Maulana Ahmad Aji Triadi <maulanaajk@gmail.com>
2025-06-05 23:44:58 +0700

    refactor: use siteConfig.title for header and image alt text

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import Container from '@/components/ui/container';
 import Grid from '@/components/ui/grid';
 import { gridItems, layouts } from '@/config/grid';
+import { siteConfig } from '@/config/site';
 
 export default function Home() {
     return (
         <>
             <Container as='header' className='flex items-center justify-between py-0'>
-                <h1 className='hidden'>Maulana Ahmad Aji Triadi</h1>
+                <h1 className='hidden'>{siteConfig.title}</h1>
             </Container>
             <main className='py-8'>
                 <Grid layouts={layouts}>
--- components/grid/description.tsx
+++ components/grid/description.tsx
@@ -1,6 +1,7 @@
 import profile from '@/public/images/profile.jpg';
 import Image from 'next/image';
 import Card from '../ui/card';
+import { siteConfig } from '@/config/site';
 
 export default function Description() {
     return (
@@ -8,7 +9,7 @@ export default function Description() {
             <div className='relative size-14 overflow-hidden rounded-full sm:size-16'>
                 <Image
                     src={profile}
-                    alt='Maulana Ahmad Aji Triadi'
+                    alt={siteConfig.title}
                     fill
                     sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
                     placeholder='blur'
