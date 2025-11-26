ce46fdcf15dbb527db9a189b7b7f757f72d76fda
anudeep-17 <anudeepsai88@gmail.com>
2025-11-26 16:52:22 -0500

    bg-color matched, usd bg-neutral-800/80 incase you want a little lighter shade use /70

--- components/grid/widgets/linkedin.tsx
+++ components/grid/widgets/linkedin.tsx
@@ -7,7 +7,7 @@ import { useTheme } from 'next-themes';
 export default function LinkedIn() {
     const { theme } = useTheme();
     return (
-        <Card className='relative flex h-full flex-col items-center justify-center bg-[#0A66C2] dark:bg-neutral-800/70'>
+        <Card className='relative flex h-full flex-col items-center justify-center bg-[#0A66C2] dark:bg-neutral-800/80'>
             <div className='absolute bottom-3 left-3'>
                 <Anchor className='cancel-drag' href='https://linkedin.com/in/maulana-ahmad' target='_blank'>
                     <FaArrowRight className='-rotate-45 transition-transform duration-300 group-hover:rotate-0' />
